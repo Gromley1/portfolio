ec2516d977e82308b9dbf0f79c18b7261732bda9
gromley111 <gromley111@gmail.com>
2024-09-28 16:51:58 -0500

    add google fonts, write about me section

--- src/features/layout/components/about/about.component.tsx
+++ src/features/layout/components/about/about.component.tsx
@@ -4,7 +4,19 @@ export const About = () => {
   return (
     <section id="about" className={styles.about}>
       <h2>About Me</h2>
-      <p>I am a UI developer with X years of experience...</p>
+      <p>
+        I am a dedicated software engineer with a passion for building
+        efficient, scalable, and user-friendly applications. With extensive
+        experience in front-end development, I specialize in crafting custom UI
+        solutions using modern frameworks like Angular and React. My expertise
+        spans greenfield projects where I’ve led teams in implementing
+        pixel-perfect designs, ensuring seamless user experiences across various
+        devices. I take pride in delivering clean, maintainable code while
+        integrating robust testing methodologies such as Jest for enhanced
+        reliability. Constantly learning and adapting, I thrive on solving
+        complex problems and transforming ideas into functional, high-quality
+        software
+      </p>
     </section>
   )
 }
