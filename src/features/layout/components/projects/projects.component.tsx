29991f354885d5debb4c9ce320304e1c46ab41d3
gromley111 <gromley111@gmail.com>
2024-11-24 16:45:50 -0600

    add projects compoennts

--- src/features/layout/components/projects/projects.component.tsx
+++ src/features/layout/components/projects/projects.component.tsx
@@ -1,16 +1,45 @@
 import styles from './projects.module.scss'
 
+interface Project {
+  id: number
+  title: string
+  description: string
+}
+
+const projects: Array<Project> = [
+  {
+    id: 1,
+    title: 'Project 1',
+    description: 'Article Search Engine',
+  },
+  {
+    id: 2,
+    title: 'Project 2',
+    description: 'Weather App',
+  },
+]
+
+const Project = ({ title, description, id }: Project) => {
+  return (
+    <div key={id} className={styles.project}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  )
+}
+
 export const Projects = () => {
   return (
     <section id="projects" className={styles.projects}>
       <h2>My Projects</h2>
-      <div className="project-list">
-        {/* Project items */}
-        <div className="project-item">
-          <h3>Project 1</h3>
-          <p>Description of the project...</p>
-        </div>
-        {/* Repeat for other projects */}
+      <div className={styles.projectList}>
+        {projects.map((project) => (
+          <Project
+            id={project.id}
+            title={project.title}
+            description={project.description}
+          />
+        ))}
       </div>
     </section>
   )
